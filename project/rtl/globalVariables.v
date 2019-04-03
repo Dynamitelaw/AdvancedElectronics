de19447fbf6823763e5547dd5950bbf2e8fffbf4
zhouziyu30 <48365037+zhouziyu30@users.noreply.github.com>
2019-04-02 20:16:21 -0400

    add buffer variables

--- project/rtl/globalVariables.v
+++ project/rtl/globalVariables.v
@@ -5,7 +5,7 @@ This file contains all global macro definitions
 `define TRUE 1
 `define FALSE 0
 
-`define DATA_WIDTH 8
+`define DATA_WIDTH 6
 
 //Network globals
 `define NETWORK_ROW_ADDRESS_WIDTH 2
@@ -16,4 +16,6 @@ This file contains all global macro definitions
 `define CACHE_BANK_ADDRESS_WIDTH 8
 `define CACHE_BANK_LINES 8
 
-
+//BUffer size and counter
+`define BUFFER_SIZE 8
+`define BIT_SIZE 3 //BUFFER_SIZE = 2^(BIT_SIZE)
